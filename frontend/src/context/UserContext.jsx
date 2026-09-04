7d27c1fa99c6651f1d88cfb36855253db7aea0b6
NethmiAyesha82 <nethmiayesha68@gmail.com>
2026-09-04 14:58:32 +0530

    new commit

--- frontend/src/context/UserContext.jsx
+++ frontend/src/context/UserContext.jsx
@@ -9,7 +9,7 @@ import axios from "axios";
 export const userDataContext = createContext();
 
 const UserContext = ({ children }) => {
-  const serverUrl = "http://localhost:8000";
+  const serverUrl = "https://virtual-assistant-85xq.vercel.app/";
 
   const [userData, setUserData] = useState(null);
   const [frontendImage, setFrontendImage] = useState(null);
